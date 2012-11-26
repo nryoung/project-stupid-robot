56f93976be39028efda7cf45249d4052448b7a76
MrQweex <mrqweex@qweex.com>
2012-11-26 16:20:51 -0700

    Added the actual code for the project

---
+++ RDK_LineSensor/RDK_LineSensor.X/build/default/production/_ext/1472/MtrCtrl.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/MtrCtrl.o:  \
+../MtrCtrl.c  \
+../config.h  \
+ ../stdtypes.h  \
+../MtrCtrl.h 
---
+++ RDK_LineSensor/RDK_LineSensor.X/build/default/production/_ext/1472/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/main.o:  \
+../main.c  \
+../stdtypes.h  \
+ ../config.h  \
+../MtrCtrl.h 
